9d3300d37ebed05075111628f3f9d313d6dc57bc
srajanConsultadd <srajan.s@consultadd.com>
2024-01-21 06:11:08 +0530

    first commit

---
+++ src/index.ts
@@ -0,0 +1,30 @@
+import { context } from "@actions/github";
+
+import { summarizeCommits } from "./commitSummary";
+import { getFilesSummaries } from "./filesSummary";
+
+async function run(): Promise<void> {
+  // Get the pull request number and repository owner and name from the context object
+  const { number } = context.payload.pull_request as {
+    number: number;
+  };
+  const issueNumber = number;
+  const { repository } = context.payload;
+
+  if (repository === undefined) {
+    throw new Error("Repository undefined");
+  }
+
+  // Create a dictionary with the modified files being keys, and the hash values of the latest commits in which the file was modified being the values
+  const modifiedFilesSummaries = await getFilesSummaries(
+    issueNumber,
+    repository
+  );
+
+  await summarizeCommits(issueNumber, repository, modifiedFilesSummaries);
+}
+
+run().catch((error) => {
+  console.error(error);
+  process.exit(1);
+});
